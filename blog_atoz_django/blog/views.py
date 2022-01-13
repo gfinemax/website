6d452a96364bbdbceb1c010835076634019f9ce2
(gfinemax) <(gfinemax@gmail.com)>
2022-01-14 08:58:13 +0900

    로그인한 사용자에게만 포스트 작성 페이지 열어주기

--- blog_atoz_django/blog/views.py
+++ blog_atoz_django/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Category, Post, Tag
 
 class PostList(ListView):
@@ -22,6 +23,19 @@ class PostDetail(DetailView):
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
 
+class PostCreate(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
+    
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated:
+            form.instance.author = current_user
+            return super(PostCreate, self).form_valid(form)
+
+        else:
+            return redirect('/blog/')    
+    
 def category_page(request, slug):
     category = Category.objects.get(slug=slug)
     if slug == 'no_category':
@@ -55,6 +69,8 @@ def tag_page(request, slug):
             'no_category_post_count': Post.objects.filter(category=None).count(),
         }
     )
+    
+
 # FBV로 실습
 # def index(request):
 #     posts = Post.objects.all().order_by('-pk')
