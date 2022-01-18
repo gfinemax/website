802695d779b5d290cfec92d0799afefd1c1b8637
(gfinemax) <(gfinemax@gmail.com)>
2022-01-18 11:09:39 +0900

    소스트리에서 올림

--- blog_atoz_django/blog/views.py
+++ blog_atoz_django/blog/views.py
@@ -12,6 +12,7 @@ class PostList(ListView):
     model = Post
     # template_name = 'blog/index.html'
     ordering = '-pk'
+    paginate_by = 5
     
     def get_context_data(self, **kwargs):
         context = super(PostList, self).get_context_data()
@@ -164,7 +165,17 @@ class CommentUpdate(LoginRequiredMixin, UpdateView):
             return super(CommentUpdate, self).dispatch(request, *args, **kwargs)
         else:
             raise PermissionDenied
-
+        
+def delete_comment(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post = comment.post
+    if request.user.is_authenticated and request.user == comment.author:
+        comment.delete()
+        return redirect(post.get_absolute_url())
+    else:
+        raise PermissionDenied
+    
+            
 # FBV로 실습
 # def index(request):
 #     posts = Post.objects.all().order_by('-pk')
