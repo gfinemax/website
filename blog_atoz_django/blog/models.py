2dd42371ba810c1dc3d63a05da97d05546dbab27
(gfinemax) <(gfinemax@gmail.com)>
2022-01-18 17:02:27 +0900

    대문 페이지 수정하기

--- blog_atoz_django/blog/models.py
+++ blog_atoz_django/blog/models.py
@@ -78,6 +78,5 @@ class Comment(models.Model):
         if self.author.socialaccount_set.exists():
             return self.author.socialaccount_set.first().get_avatar_url()
         else:
-            # return 'http://placehold.it/50*50'
             return f'https://doitdjango.com/avatar/id/부여받은 id/부여받은 key/svg/{self.author.email}'
         
